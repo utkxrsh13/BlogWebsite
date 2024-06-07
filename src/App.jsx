b2ce293b97da92f022ba2de89d34c575fc81fa9a
utkxrsh13 <utkarsht0813@gmail.com>
2024-06-07 10:18:41 +0530

    Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
 
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from "./appwite/auth"
+import {login, logout} from "./store/authslice"
+import {Footer , Header} from "./components"
+import { Outlet } from 'react-router-dom'
+
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  // console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>A blog page</h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser().then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+        
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'> 
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
